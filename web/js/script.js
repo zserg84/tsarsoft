adae253c75d123976a5cf6ca147ed43d464cf877
Сергей <sz@smartarenda.ru>
2015-01-27 13:13:01 +0500

    js css

--- web/js/script.js
+++ web/js/script.js
@@ -2,21 +2,7 @@ $(document).ready(function() {
 
 	$('select').selectric();
 	
-	if ( $('div').is('#fullpage') ){
-		$('#fullpage').fullpage({
-			anchors: ['window-0', 'window-1', 'window-2', 'window-3', 'window-4', 'window-5', 'window-6'],
-			menu: '#myMenu',
-			css3: true,
-			animateAnchor: true,
-			onLeave: function() {
-				if ($('#section0').is('.active')) {
-					$('.main-menu').removeClass('top').addClass('bottom');
-				} else {
-					$('.main-menu').removeClass('bottom').addClass('top');
-				}
-			}
-		});
-	};
+
 	$('.portfolio-elem').children('.title').bind('click', function(){
 		$(this).parent('.portfolio-elem').addClass('active');
 	});
@@ -28,4 +14,37 @@ $(document).ready(function() {
 		scrolling: 'visible',
 		wrapCSS: 'wrap-popup'
 	});
+	function initFullpage(){
+		var width = $( window ).width(),
+			height = $( window ).height();
+
+		if ( $('div').is('#fullpage') ){
+			if( height > 400 ) {
+				if ( height / width > 0.5 ) {
+					$('#fullpage').fullpage({
+						anchors: ['window-0', 'window-1', 'window-2', 'window-3', 'window-4', 'window-5', 'window-6'],
+						menu: '#myMenu',
+						css3: true,
+						animateAnchor: true,
+						onLeave: function () {
+							if ($('#section0').is('.active')) {
+								$('.main-menu').removeClass('top').addClass('bottom');
+							} else {
+								$('.main-menu').removeClass('bottom').addClass('top');
+							}
+						}
+					});
+				}
+			} else {
+				$.fn.fullpage.destroy('all');
+			};
+		}
+	};
+
+	initFullpage();
+
+	$( window ).resize(function() {
+		initFullpage();
+	});
+
 })
