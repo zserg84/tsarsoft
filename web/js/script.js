a7c2c92a67e7a393237109b50ddbd68901ecba04
Сергей <sz@smartarenda.ru>
2015-01-28 11:06:13 +0500

    js

--- web/js/script.js
+++ web/js/script.js
@@ -15,29 +15,24 @@ $(document).ready(function() {
 		wrapCSS: 'wrap-popup'
 	});
 	function initFullpage(){
-		var width = $( window ).width(),
-			height = $( window ).height();
+		var height = $( window ).height();
 
 		if ( $('div').is('#fullpage') ){
-			if( height > 400 ) {
-				if ( height / width > 0.5 ) {
-					$('#fullpage').fullpage({
-						anchors: ['window-0', 'window-1', 'window-2', 'window-3', 'window-4', 'window-5', 'window-6'],
-						menu: '#myMenu',
-						css3: true,
-						animateAnchor: true,
-						onLeave: function () {
-							if ($('#section0').is('.active')) {
-								$('.main-menu').removeClass('top').addClass('bottom');
-							} else {
-								$('.main-menu').removeClass('bottom').addClass('top');
-							}
+			if( height > 700 ) {
+				$('#fullpage').fullpage({
+					anchors: ['window-0', 'window-1', 'window-2', 'window-3', 'window-4', 'window-5', 'window-6'],
+					menu: '#myMenu',
+					css3: true,
+					animateAnchor: true,
+					onLeave: function () {
+						if ($('#section0').is('.active')) {
+							$('.main-menu').removeClass('top').addClass('bottom');
+						} else {
+							$('.main-menu').removeClass('bottom').addClass('top');
 						}
-					});
-				}
-			} else {
-				$.fn.fullpage.destroy('all');
-			};
+					}
+				});
+			}
 		}
 	};
 
